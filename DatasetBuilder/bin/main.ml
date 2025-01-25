b7e152350c40a66678d0ccccdbbdd152ee532f14
Krzysztof Wasielewski <krzychuwas@gmail.com>
2025-01-25 13:42:15 +0100

    Songs in relative values
    Signed-off-by: Krzysztof Wasielewski <krzychuwas@gmail.com>

--- DatasetBuilder/bin/main.ml
+++ DatasetBuilder/bin/main.ml
@@ -33,5 +33,31 @@ let print_song = function
       print_endline "Song";
       print_bars bs
 
+
+let rec print_rel_notes = function
+| [] -> ()
+| RelNote (i, l) :: rest ->
+    print_string "Note ";
+    print_int i;
+    print_newline ();
+    print_string "Len ";
+    if fst l then print_char '/';
+    print_int (snd l);
+    print_newline ();
+    print_rel_notes rest
+
+let rec print_rel_bars = function
+| [] -> ()
+| RelBar ns :: rest ->
+    print_endline "Bar";
+    print_rel_notes ns;
+    print_rel_bars rest
+
+let print_rel_song = function
+| RelSong bs ->
+    print_endline "Song";
+    print_rel_bars bs
 let song = sheet token (Lexing.from_channel stdin)
-let _ = print_song (snd song)
+(*let _ = print_song (snd song)*)
+
+let _ = print_rel_song (rel_song_of_song (fst song) (snd song))
--- DatasetBuilder/bin/song.ml
+++ DatasetBuilder/bin/song.ml
@@ -1,13 +1,60 @@
+(* absolute notes *)
 type accidental = char option
 type octave_shift = int
 type length = bool * int
 type note = Note of accidental * char * octave_shift * length
 type bar = Bar of note list
 type song = Song of bar list
-type rel_note = RelNote of int
+
+(* notes relative to key *)
+type rel_note = RelNote of int * length
 type rel_bar = RelBar of rel_note list
 type rel_song = RelSong of rel_bar list
 
 module SongConfig = Map.Make (Char)
 
-let rel_song_of_song cfg s = ()
+let note_to_semitone = function
+  | 'C' -> 0
+  | 'D' -> 2
+  | 'E' -> 4
+  | 'F' -> 5
+  | 'G' -> 7
+  | 'A' -> 9
+  | 'B' -> 11
+  | 'c' -> 12
+  | 'd' -> 14
+  | 'e' -> 16
+  | 'f' -> 17
+  | 'g' -> 19
+  | 'a' -> 21
+  | 'b' -> 23
+  | c -> failwith @@ "Invalid note" ^ (String.make 1 c)
+
+let is_uppercase c = Char.uppercase_ascii c = c
+
+let rel_song_of_song (cfg : string SongConfig.t) (s : song) : rel_song =
+  let key = SongConfig.find 'K' cfg in
+  let offset = note_to_semitone key.[2] (*to do sharps and flats*) in
+  let offset =
+    if String.length key > 3 then
+      if key.[4] == '#' then offset + 1 else offset - 1
+    else offset
+  in
+  let map_note = function
+    | Note (a, c, octave_shift, l) ->
+        let modifier = octave_shift * if is_uppercase c then -12 else 12 in
+        let modifier =
+          modifier
+          +
+          match a with
+          | None -> 0
+          | Some '^' -> 1
+          | Some '_' -> -1
+          | Some '=' -> failwith "naturals uninmplemented"
+          | Some c -> failwith @@ "incorrect accidental" ^ String.make 1 c
+        in
+        RelNote
+          (modifier - offset + note_to_semitone (Char.uppercase_ascii c), l)
+  in
+  let map_bar = function Bar x -> RelBar (List.map map_note x) in
+  match s with Song s -> RelSong (List.map map_bar s)
