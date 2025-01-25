f8e3000154acd77cd3a2da8bc3b038ac7f54cfec
Krzysztof Wasielewski <krzychuwas@gmail.com>
2025-01-25 12:22:20 +0100

    Added OCaml formatting
    Signed-off-by: Krzysztof Wasielewski <krzychuwas@gmail.com>

--- DatasetBuilder/bin/main.ml
+++ DatasetBuilder/bin/main.ml
@@ -5,23 +5,33 @@ open Song
 
 (* print productions *)
 let rec print_notes = function
-  | [] -> () 
-  | Note (a, n, i, l) :: rest 
-    -> print_string "Note ";
-    (match a with 
-      | Some v -> print_char v;
-      | _ -> ()); 
+  | [] -> ()
+  | Note (a, n, i, l) :: rest ->
+      print_string "Note ";
+      (match a with Some v -> print_char v | _ -> ());
       print_char n;
       print_newline ();
-      (if i != 0 then
-         (print_string "Shift "; print_int i; print_newline ()));
-      print_string "Len "; (if fst l then print_char '/'); print_int (snd l); print_newline (); 
+      if i != 0 then (
+        print_string "Shift ";
+        print_int i;
+        print_newline ());
+      print_string "Len ";
+      if fst l then print_char '/';
+      print_int (snd l);
+      print_newline ();
       print_notes rest
+
 let rec print_bars = function
-  | [] ->  ()
-  | Bar ns :: rest -> print_endline "Bar"; print_notes ns; print_bars rest
-let print_song = function 
-  | Song bs -> print_endline "Song"; print_bars bs
+  | [] -> ()
+  | Bar ns :: rest ->
+      print_endline "Bar";
+      print_notes ns;
+      print_bars rest
+
+let print_song = function
+  | Song bs ->
+      print_endline "Song";
+      print_bars bs
 
 let song = sheet token (Lexing.from_channel stdin)
 let _ = print_song (snd song)
--- DatasetBuilder/bin/song.ml
+++ DatasetBuilder/bin/song.ml
@@ -2,16 +2,12 @@ type accidental = char option
 type octave_shift = int
 type length = bool * int
 type note = Note of accidental * char * octave_shift * length
-type bar  = Bar of note list
+type bar = Bar of note list
 type song = Song of bar list
-
-
 type rel_note = RelNote of int
-type rel_bar  = RelBar of rel_note list
+type rel_bar = RelBar of rel_note list
 type rel_song = RelSong of rel_bar list
 
-module SongConfig = Map.Make(Char)
+module SongConfig = Map.Make (Char)
 
-
-let rel_song_of_song cfg s =
-  ()
+let rel_song_of_song cfg s = ()
