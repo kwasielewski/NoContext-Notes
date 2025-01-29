f07ed10083bed2433eaca6b301ac6f643acdf53a
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2025-01-29 18:25:20 +0100

    konwersja plików joł:

---
+++ abc_work.py
@@ -0,0 +1,32 @@
+import json
+import os
+from datetime import datetime
+from subprocess import DEVNULL, run, PIPE
+
+with open("train.json") as tf:
+    songs = json.load(tf)
+
+count = 0
+wrong_songs = []
+len_ = 1000
+
+catalog_name = f'run{datetime.now().strftime("%Y-%m-%d%H-%M-%S")}'
+os.mkdir(catalog_name)
+for idx, song in enumerate(songs[:len_]):
+    p = run(["DatasetBuilder/_build/default/bin/main.exe"], stdout=PIPE, stderr=DEVNULL, input=song["abc notation"], encoding='ascii')
+    count += 1 if p.returncode != 0 else 0
+
+    if p.returncode == 0:
+        with open(f"{catalog_name}/{idx}.abc", "w") as f:
+            f.write(p.stdout)
+    else:
+        wrong_songs += [song]
+    
+    if idx % 100 == 0:
+        print(idx)
+
+print(f"Res: {len_ - count}/{len_}")
+print(f"Res: {(len_ - count) * (len(songs) / len_)}")
+
+with open(f'{catalog_name}/bad.json', 'w') as f:
+    json.dump(wrong_songs, f)
